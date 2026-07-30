f441a37838ed03e740cb2d2cca0e540424aa74f5
kamyk1544444 <footballenjoyer1544@gmail.com>
2026-07-31 00:05:04 +0200

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.py
@@ -0,0 +1,18 @@
+
+import sys
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [sys.maxsize] * (amount+1)
+
+        dp[0] = 0
+
+        for i in range(1,amount+1):
+            for coin in coins:
+
+                if i-coin >= 0:
+                    dp[i] = min(dp[i],dp[i-coin]+1)
+        
+        return dp[amount] if dp[amount] != sys.maxsize else -1
+                  
