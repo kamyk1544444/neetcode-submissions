f3273c69dd7171d1f81e6fce93fae418375d6bfd
kamyk1544444 <footballenjoyer1544@gmail.com>
2026-08-17 07:35:03 +0200

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        
+        res = ""
+
+        for slowo in strs:
+            res += str(len(slowo)) + "*" + slowo
+        
+        return res
+
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+
+        start = 1
+        i=0
+        
+        while i < len(s)-1:
+            
+            i +=1
+            if s[i] != "*":
+                start +=1
+                continue
+            
+            lenght = int(s[i-start:i])
+            res.append(s[i+1:i+lenght+1])
+            i = i+lenght
+            start = 0
+            
+            
+        return res
