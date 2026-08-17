495708556566901e0b40900c68a7e10b55684954
kamyk1544444 <footballenjoyer1544@gmail.com>
2026-08-17 08:06:11 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        left, right = 0,len(s)-1
+        space = ord(" ")
+
+        while right>left:
+
+            if not s[left].isalnum():
+                left +=1
+                continue
+            elif not s[right].isalnum():
+                right -=1
+                continue
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            left +=1
+            right -=1
+        return True
