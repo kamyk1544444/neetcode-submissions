acb348a8f17d511eb0457936a10eca70320efc01
kamyk1544444 <footballenjoyer1544@gmail.com>
2026-08-22 08:55:39 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        buy = prices[0]
+
+        res = 0
+
+        for p in prices[1:]:
+
+            res = max(res,p-buy)
+
+            buy = min(buy,p)
+            
+        return res
